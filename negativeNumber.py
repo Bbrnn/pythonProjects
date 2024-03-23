39fd71e138bd1d5ac58fe3539136d298f1669103
Bbrnn <ngugi.njoki21@students.dkut.ac.ke>
2024-03-23 09:04:27 +0300

    first commit

---
+++ negativeNumber.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 6
+if number1 != number2:
+    number2 = ++number1#this is equivalent to number2=number1
+    print(number1 - number2)
+
+    number2 = number1 + 1
+    print(number1 - number2)
